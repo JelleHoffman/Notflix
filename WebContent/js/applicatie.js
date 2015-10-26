29bd8dd413a539118d099c18e1a3e2bcdd0ae231
Peddz24 <jelle.hoffman@hotmail.com>
2015-10-26 13:32:31 +0100

    Layout shit

--- WebContent/js/applicatie.js
+++ WebContent/js/applicatie.js
@@ -9,3 +9,98 @@ function singIn(){
 			});
 	});
 }
+
+function getFilms(){
+	$(document).ready(function(){
+			var content = $("#mainContent");
+			var accessToken = localStorage.getItem("accessToken");
+			var apiUrl;
+			var urlHeaders;
+			
+			alert(accessToken);
+			
+			if(accessToken==null){
+				alert("in de if");
+				apiUrl ="./api/film/ratings";
+				urlHeaders = {
+					Accept:"application/json; charset=utf-8",
+				};
+			}else{
+				alert("in de else");
+				apiUrl ="./api/film";
+				urlHeaders = {
+					Accept:"application/json; charset=utf-8",
+					Authorization:accessToken,
+				};
+			};
+			
+			$.ajax({
+				type:"GET",
+				url:apiUrl,
+				headers:urlHeaders,
+				success:function(data){
+					$.each(data,function(index,value){
+						var title = value.titel;
+						var imdbId = value.iMDBNummer;
+						var poster;
+						
+						$.ajax({
+							type:"GET",
+							url:("http://www.omdbapi.com/?i="+imdbId),
+							success:function(data){
+								
+								poster = data.Poster;
+								
+								if(title.length>12){
+									var shortTitle = title.substr(0,12)+"...";
+								
+								}else{
+									var shortTitle = title;
+								}
+								//film object toevoegen
+								content.append(
+									"<div class='filmFrame col-md-3'>"+
+									"<img id='filmPoster' class='col-md-10' alt='"+title+"'src="+poster+">"+
+									
+									"<h2 id='title'>"+shortTitle+"</h2>"+
+									"<h3 id='regiseur'>"+value.regisseur+"</h3>"+
+									"<h4 id='time'>Minutes: "+value.duur+"</h4>"+
+									"<h4 id='releaseDate'>Release date: "+value.datum+"</h4>"+
+									"<h4 id='average'>Rating: "+value.average+"</h4>"+
+									"<div class='stars'>"+
+						  			"<form action=''>"+
+								    "<input class='star star-5' id='star-5' type='radio' name='star'/>"+
+								    "<label class='star star-5' for='star-5'></label>"+
+								  	"<input class='star star-4' id='star-4' type='radio' name='star'/>"+
+							    "<label class='star star-4' for='star-4'></label>"+
+							    "<input class='star star-3' id='star-3' type='radio' name='star'/>"+
+								    "<label class='star star-3' for='star-3'></label>"+
+								  	"<input class='star star-2' id='star-2' type='radio' name='star'/>"+
+							    "<label class='star star-2' for='star-2'></label>"+
+							    "<input class='star star-1' id='star-1' type='radio' name='star'/>"+
+								    "<label class='star star-1' for='star-1'></label>"+
+						 			"</form>"+
+									"</div>"+
+									"</div>");
+								
+								//rating meegeven
+								//var rating = value.average;
+								//if(value>==1){
+									
+								//}
+									
+							},
+							error:function(jqXHR,settings,error){
+								alert(error);
+							}
+						});
+						
+						
+					});
+				},
+				error:function(jqXHR,settings,error){
+					alert("error in getFilms"+error+settings);
+				}
+			});
+		});
+}
--- src/model/Model.java
+++ src/model/Model.java
@@ -15,8 +15,8 @@ public class Model {
 		gebruikers.add(new Gebruiker("Luuk", "", "Wellink", "12345", "sportief pookje"));
 		films.add(new Movie(films.size()+1, "tt0105236", "Reservoir Dogs", "5-8-1993", 99, "Quentin Tarantino", "After a simple jewelery heist goes terribly wrong, the surviving criminals begin to suspect that one of them is a police informant."));
 		Movie b = (new Movie(films.size()+1, "tt0111161", "The Shawshank Redemption", "2-3-1994",142, "Frank Darabont","Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."));
-		films.add(new Movie(films.size()+1, "tt0068646", "The Godfather", "18-1-1973",175,"Francis Ford Coppola","The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."));
-		films.add(new Movie(films.size()+1, "tt0468569","The Dark Knight", "24-7-2008", 152,"Christopher Nolan","When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice."));
+		Movie d = (new Movie(films.size()+1, "tt0068646", "The Godfather", "18-1-1973",175,"Francis Ford Coppola","The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."));
+		Movie f = (new Movie(films.size()+1, "tt0468569","The Dark Knight", "24-7-2008", 152,"Christopher Nolan","When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice."));
 		films.add(new Movie(films.size()+1, "tt0050083", "Angry men","1-4-1957",96,"Sidney Lumet","A dissenting juror in a murder trial slowly manages to convince the others that the case is not as obviously clear as it seemed in court."));
 		films.add(new Movie(films.size()+1, "tt0108052", "Schindler's List","3-3-1994",195,"Steven Spielberg","In Poland during World War II, Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis."));
 		films.add(new Movie(films.size()+1, "tt0110912","Pulp Fiction","1-12-1994",154,"Quentin Tarantino","The lives of two mob hit men, a boxer, a gangster's wife, and a pair of diner bandits intertwine in four tales of violence and redemption."));
@@ -26,9 +26,13 @@ public class Model {
 		m.addRating(new Rating(1, gebruikers.get(1)));
 		n.addRating(new Rating(2, gebruikers.get(1)));
 		b.addRating(new Rating(3, gebruikers.get(1)));
+		d.addRating(new Rating(4, gebruikers.get(1)));
+		f.addRating(new Rating(2,gebruikers.get(1)));
 		films.add(m);
 		films.add(n);
 		films.add(b);
+		films.add(d);
+		films.add(f);
 		
 	}
 	public ArrayList<Gebruiker> getGebruikers() {
